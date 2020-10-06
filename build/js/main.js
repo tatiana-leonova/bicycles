3efc39a8fe45debea1daa1b04588cb1b99100499
Tatiana <lts221@yandex.ru>
2020-10-06 17:07:15 +0300

    Правки в меню

--- build/js/main.js
+++ build/js/main.js
@@ -4,8 +4,9 @@
 
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var body = document.querySelector('.body');
 
-function changeState() {
+function changeStateMenu() {
   navMain.classList.toggle('main-nav--closed');
   navMain.classList.toggle('main-nav--opened');
 }
@@ -14,8 +15,15 @@ if (navMain && navToggle) {
   navMain.classList.remove('main-nav--nojs');
 
   navToggle.onclick = function () {
-    changeState();
+    changeStateMenu();
+    body.classList.toggle('body__js-menu');
+    // if (body.classList.contains('body__js-menu')) {
+    //   body.classList.remove('body__js-menu');
+    // } else {
+    //   body.classList.add('body__js-menu');
+    // }
+
   };
 
-  changeState();
+  changeStateMenu();
 }
--- source/js/main.js
+++ source/js/main.js
@@ -4,8 +4,9 @@
 
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var body = document.querySelector('.body');
 
-function changeState() {
+function changeStateMenu() {
   navMain.classList.toggle('main-nav--closed');
   navMain.classList.toggle('main-nav--opened');
 }
@@ -14,8 +15,9 @@ if (navMain && navToggle) {
   navMain.classList.remove('main-nav--nojs');
 
   navToggle.onclick = function () {
-    changeState();
+    changeStateMenu();
+    body.classList.toggle('body__js-menu');
   };
 
-  changeState();
+  changeStateMenu();
 }
